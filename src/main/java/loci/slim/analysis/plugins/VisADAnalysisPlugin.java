0e244a7e61cf484749cfed22f84f42b8d7689219
Aivar Grislis <grislis@wisc.edu>
2010-11-13 03:05:51 +0000

    A VisAD analysis plugin.

---
+++ src/main/java/loci/slim/analysis/plugins/VisADAnalysisPlugin.java
@@ -0,0 +1,34 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package loci.slim.analysis.plugins;
+
+import ij.IJ;
+import ij.ImagePlus;
+
+import imagej.process.ImageUtils;
+
+import loci.slim.SLIMProcessor.FitFunction;
+import loci.slim.SLIMProcessor.FitRegion;
+import loci.slim.analysis.ISLIMAnalyzer;
+import loci.slim.analysis.SLIMAnalyzer;
+
+import mpicbg.imglib.image.Image;
+import mpicbg.imglib.type.numeric.real.DoubleType;
+
+/**
+ * Runs the VisADPlugin to analyze SLIMPlugin results.
+ *
+ * This will run on a separate thread, control returns to the caller right away.
+ *
+ * @author Aivar Grislis
+ */
+@SLIMAnalyzer(name="VisAD")
+public class VisADAnalysisPlugin implements ISLIMAnalyzer {
+    public void analyze(Image<DoubleType> image, FitRegion region, FitFunction function) {
+        ImagePlus imp = ImageUtils.createImagePlus(image, "Fitted results");
+        IJ.runPlugIn(imp, "imagej.visad.VisADPlugin", "");
+    }
+}
