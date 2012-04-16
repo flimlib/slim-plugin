aa3dfcb48fb1e8feca976ce169122b5fd248879b
Aivar Grislis <grislis@wisc.edu>
2012-04-16 03:38:26 +0000

    SLIM Plugin: Further work on masking.

---
+++ src/main/java/loci/slim/mask/IMaskNodeListener.java
@@ -0,0 +1,16 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package loci.slim.mask;
+
+/**
+ *
+ * @author aivar
+ */
+public interface IMaskNodeListener {
+
+    public void updateMask(Mask mask);
+    
+}
