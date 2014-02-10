2c08f4d1a62d73166116d420b121aa940c3a91fe
Curtis Rueden <ctrueden@wisc.edu>
2014-02-10 06:57:48 -0600

    SLIMProcessor: remove unneeded import

--- src/main/java/loci/slim/SLIMProcessor.java
+++ src/main/java/loci/slim/SLIMProcessor.java
@@ -40,7 +40,6 @@ import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Hashtable;
 import java.util.List;
 import java.util.Map;
 import java.util.prefs.Preferences;
